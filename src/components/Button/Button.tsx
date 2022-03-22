67f1046c1f0279d131858162006687e70de5c885
Johan 'Anders' Ramsay <819213+andersr@users.noreply.github.com>
2022-03-22 15:20:35 -0400

    Add button styling

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,24 @@
 import React from "react";
+import styled from "styled-components";
 
-export const Button: React.FC = ({ children }) => {
-  return <button>{children}</button>;
-};
+interface Props {
+  disabled?: boolean;
+}
+
+export const Button = styled.button<Props>`
+  appearance: none;
+   border-radius: 0.12em;
+   padding: 0.5em 1.4em;
+  font-size: 22px;
+  font-weight: normal;
+  background-color: white;
+  color: black;
+  border: 1px solid #000;
+
+  ${({ disabled }) =>
+    disabled &&
+    `
+    color: #ccc;
+    border: 1px solid #666;
+  `}
+`;
